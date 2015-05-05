c0017fc2ee4b117e0b487a40d2aec2a1e4358b8c
bmwilly <brandon.williams@kineticsocial.com>
2015-05-05 15:20:40 -0400

    asdf

--- prob3.jl
+++ prob3.jl
@@ -2,48 +2,22 @@
 
 # What is the largest prime factor of the number 600851475143 ?
 
-
-function is_prime(n::Int64)
-
-    # check for 1, 2, 3
-    if n <= 3
-        return true
+function lpfun(x)
+  lpf = 2
+  while (x > lpf)
+    if (x % lpf == 0)
+      x = x/lpf
+      lpf = 2
+    else
+      lpf += 1
     end
-
-    # check if number is even
-    if n % 2 == 0
-        return false
-    end
-
-    # initialize counter variable
-    i = 3
-
-    # only need to check til sqrt(n)
-    while i <= sqrt(n)
-
-        # check for divisibility
-        if n % i == 0
-            return false
-        end
-
-        i += 2
-    end
-
-    # otherwise the number is prime
-    return true
+  end
+  lpf
 end
 
-
-num = 600851475143
-maxprime = 1
-
-for n = 1:sqrt(num)
-    if is_prime(n)
-        if n > maxprime
-            maxprime = n
-        end
-    end
+function main(bigint)
+  ans = lpfun(bigint)
+  println("The largest prime factor of $(bigint) is $(ans)")
 end
 
-maxprime
-
+main(600851475143)
